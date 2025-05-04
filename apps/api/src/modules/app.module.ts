5a2bdae59d88c927b78224a32aa97fbb6f8c1f8d
revinto <jakub.gornikk@gmail.com>
2025-05-04 16:21:52 +0200

    feat: add supabase service

--- apps/api/src/modules/app.module.ts
+++ apps/api/src/modules/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule, PrismaService } from '@packages/db';
 import { UserModule } from './user/user.module';
-
+import { SupabaseModule } from './supabase/supabase.module';
+import { SupabaseService } from './supabase/supabase.service';
+import * as path from 'path';
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, '../../.env'), // Use absolute path
+    }),
+    SupabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, SupabaseService],
 })
 export class AppModule {}
