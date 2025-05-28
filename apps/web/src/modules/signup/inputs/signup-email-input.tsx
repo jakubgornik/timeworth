4541f4b41c97b5d808f853c72251c7eb65353d04
revinto <jakub.gornikk@gmail.com>
2025-05-28 19:31:58 +0200

    ...

---
+++ apps/web/src/modules/signup/inputs/signup-email-input.tsx
@@ -0,0 +1,45 @@
+import { Control, FieldPath, FieldValues } from "react-hook-form";
+import { AnimatePresence } from "motion/react";
+
+import { Input } from "@/components/ui/input";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import FormInputError from "@/components/form-input-error";
+
+interface SignupEmailInputProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> {
+  control: Control<TFieldValues>;
+  name: TName;
+  error?: string;
+}
+
+export default function SignupEmailFieldInput<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({ control, name, error }: SignupEmailInputProps<TFieldValues, TName>) {
+  return (
+    <FormField
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Email</FormLabel>
+          <FormControl>
+            <Input placeholder="example@email.com" {...field} />
+          </FormControl>
+          <div className="min-h-[1.5rem]">
+            <AnimatePresence>
+              {error && <FormInputError message={String(error)} />}
+            </AnimatePresence>
+          </div>
+        </FormItem>
+      )}
+    />
+  );
+}
