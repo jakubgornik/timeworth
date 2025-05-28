13e5868af61598e0996a0cc8af92c70520ce57cf
revinto <jakub.gornikk@gmail.com>
2025-05-28 19:31:58 +0200

    ...

--- apps/web/src/modules/signup/signup-form-card.tsx
+++ apps/web/src/modules/signup/signup-form-card.tsx
@@ -7,7 +7,7 @@ import SignUpFormCardInfo from "./signup-form-card-info";
 
 export function SignUpFormCard() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-4">
+    <div className="flex items-center justify-center min-h-screen">
       <motion.div
         initial="hidden"
         animate="visible"
