348b7aff3617ea7867a7074f3728492179ac5cca
revinto <jakub.gornikk@gmail.com>
2025-06-12 23:00:53 +0200

    ...

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -3,8 +3,8 @@ import Layout from "@/components/layout";
 export default function DashboardPage() {
   return (
     <Layout>
-      <header className="px-4 md:px-0 md:pl-4 h-16 py-4 w-full border-b bg-primary">
-        <h1 className="text-2xl font-bold">Dashboard Page</h1>
+      <header className="px-4 md:px-0 md:pl-4 h-16 py-4 w-full border-b bg-background md:bg-primary">
+        <h1 className="md:text-2xl text-lg font-bold">Dashboard Page</h1>
       </header>
       <div className="md:px-2 md:py-2 w-full h-full flex justify-center items-center">
         <h1 className="text-2xl font-bold">Dashboard Page content</h1>
