199c881334ca951b5adb112ecae61a597c20d7c8
revinto <jakub.gornikk@gmail.com>
2025-07-05 22:59:16 +0200

    ...

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -23,6 +23,12 @@ export default function DashboardPage() {
     [currentUser.data?.organization?.managerId, currentUser.data?.id]
   );
   console.log(userIsManager);
+  // TODO:
+  // 1. seed data with users to display
+  // 2. pass data to hook to create details jsx for each row, pass data, columns, and renderExpandedRow to table
+  // 3. adjust table styles to fit app layout, fix responsivness
+  // 4. add loading to table dispaly loader while fetching data
+  //  .. and much more
   const { columns, data, renderExpandedRow } = useTableDemo();
 
   return (
@@ -30,34 +36,34 @@ export default function DashboardPage() {
       <SectionHeader title="Dashboard Page" />
       <SectionWrapper className="h-full">
         <Card className="w-full h-full bg-primary">
-          <CardHeader className="text-2xl font-bold flex-col text-center flex gap-2 items-center justify-center mb-4">
-            Welcome to the Dashboard
-          </CardHeader>
           {!userIsOnboarded ? (
-            <CardContent className="flex flex-col lg:flex-row justify-around gap-5 px-4 sm:px-8 lg:px-12">
-              <DialogCard
-                title="Are you part of an Organization?"
-                description="To access dashboard, please join an organization. If you don't have an organization code, please contact your organization manager."
-                dialog={<JoinOrganizationDialog />}
-              />
+            <>
+              <CardHeader className="text-2xl font-bold flex-col text-center flex gap-2 items-center justify-center mb-4">
+                Welcome to the Dashboard
+              </CardHeader>
+              <CardContent className="flex flex-col lg:flex-row justify-around gap-5 px-4 sm:px-8 lg:px-12">
+                <DialogCard
+                  title="Are you part of an Organization?"
+                  description="To access dashboard, please join an organization. If you don't have an organization code, please contact your organization manager."
+                  dialog={<JoinOrganizationDialog />}
+                />
 
-              <DialogCard
-                title="Are you Organization manager?"
-                description="If you are the manager of an organization, you can create a new organization and invite members to join. Please provide the necessary details to set up your organization."
-                dialog={<CreateOrganizationDialog />}
-              />
-            </CardContent>
+                <DialogCard
+                  title="Are you Organization manager?"
+                  description="If you are the manager of an organization, you can create a new organization and invite members to join. Please provide the necessary details to set up your organization."
+                  dialog={<CreateOrganizationDialog />}
+                />
+              </CardContent>
+            </>
           ) : (
-            <div className="flex w-full h-full p-6">
+            <div className="flex w-full h-full px-6">
               <DataTable
                 data={data}
                 columns={columns}
                 pageSize={10}
                 enableExpanding={true}
                 renderExpandedRow={renderExpandedRow}
-                getRowCanExpand={(person) =>
-                  !!(person.bio || person.skills || person.department)
-                }
+                getRowCanExpand={() => true}
               />
             </div>
           )}
