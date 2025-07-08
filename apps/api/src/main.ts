ac384f0b1c78f91bd65d7ff099975457a8f0a8d1
revinto <jakub.gornikk@gmail.com>
2025-07-08 22:51:08 +0200

    ...

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
   app.use(cookieParser());
 
