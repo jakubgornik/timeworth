37a9a5ba55b0f3b62e47151c5c48db26a42bfe66
revinto <jakub.gornikk@gmail.com>
2025-07-17 00:17:46 +0200

    feat: add server side sorting and pagination

--- packages/types/src/utils/utils.types.ts
+++ packages/types/src/utils/utils.types.ts
@@ -1,5 +1,17 @@
-export interface PaginatedResponse<TData> {
+export interface IPaginatedResponseDto<TData> {
   data: TData[];
   totalCount: number;
   totalPages: number;
 }
+
+export interface IPaginationDto {
+  page: number;
+  pageSize: number;
+}
+
+export interface ISortDto {
+  id?: string;
+  desc?: boolean;
+}
+
+export interface IPaginatedQueryDto extends IPaginationDto, ISortDto {}
