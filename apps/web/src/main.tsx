d8b4bd8f56ca3c5a7ffa1a37af83e84e5be4805b
revinto <jakub.gornikk@gmail.com>
2025-09-29 22:30:03 +0200

    feat: add sentry to web app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,6 +6,16 @@ import { queryClient } from "./lib/query-client.ts";
 import "./index.css";
 import { router } from "./routes/router.tsx";
 import { NotificationProvider } from "./hooks/use-notification.tsx";
+import * as Sentry from "@sentry/react";
+
+if (import.meta.env.VITE_NODE_ENV === "production") {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    environment: import.meta.env.VITE_NODE_ENV,
+    tracesSampleRate: 0.7,
+    integrations: [Sentry.browserTracingIntegration()],
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
